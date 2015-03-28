c80cbdce160869f433328ec0fcc57a2de7fd7c8c
luzicheng <jslzc1990@163.com>
2015-03-28 23:48:50 +0800

    fix bugs

--- srvframe/base.py
+++ srvframe/base.py
@@ -18,7 +18,7 @@ class LoginBaseHandler(tornado.web.RequestHandler):
     def prepare(self):
         token = self.get_secure_cookie('manager_pic')
         if token in AuthConfig.authed_info:
-            self.user = AuthConfig.authed_info[token]['user']
+            self.user = AuthConfig.authed_info[token]
             return
         self.redirect("/login")
 
@@ -36,7 +36,7 @@ class LoginHandler(tornado.web.RequestHandler):
         return manage_pic_db
 
     def get(self):
-        self.render("login.html")
+        self.render("login.html",msg=None)
 
     def post(self):
         username=self.get_argument('username','')
@@ -48,45 +48,19 @@ class LoginHandler(tornado.web.RequestHandler):
             key = str(int(random.random() *10**16))
             self.set_secure_cookie('manage_pic',key, expires_day=2)
             AuthConfig.authed_info[key]=res
-
-
-
-
-
-
-
-        if repassword is None:
-            return self.write("用户名错误")
-        elif repassword != password:
-            return self.write("密码错误")
+            self.user=res
+            self.redirect("/")
         else:
-            return self.write("登录成功")
+            self.render("login.html",msg="用户名或者密码错误")
 
-                key = str(int(random.random() * 10**16))
-                self.set_secure_cookie('official_msg',key,expires_days=AuthConfig.duration)
-                params['user']['type'] = res['type']
-                AuthConfig.authed_info.set(key, json.dumps(params))
-                AuthConfig.authed_info.expire(key, 3600) # 1小时后超时
-                self.redirect("/")
-            else:
-            	self.render("login1.html",error_msg='用户名或密码错误，请重新登陆.',username=username)
-        except Exception as e:
-            print "Something wrong with server, please try later.", e
 
 
 class LogoutHandler(LoginBaseHandler):
     def get(self):
         token = self.get_secure_cookie('official_msg')
-        AuthConfig.authed_info.delete(token)
+        del AuthConfig.authed_info[token]
         self.clear_cookie('official_msg')
         self.redirect('/login')
 
 
-class ErrorHandler(LoginBaseHandler):
-    def get(self):
-        self.write_error(404)
-    def write_error(self,status_code,**kwargs):
-        if status_code==404:
-            self.render('404.html')
-        else:
-            self.write('error:'+str(status_code))
+
